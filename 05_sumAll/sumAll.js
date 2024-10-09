33e16af9323ef41b1d8bb73f48db40056952dd04
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-10-09 11:58:22 +0100

    Solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, lastNumber) {
+    let result = 0;
+    if (firstNumber < 0 || lastNumber < 0 || !Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)) {
+        return "ERROR";
+    } else if (firstNumber < lastNumber) {
+       for (let i = firstNumber; i <= lastNumber; i++){
+            result += i;
+        }
+        return result;
+    } else {
+        for (let i = firstNumber; i >= lastNumber; i--){
+            result += i;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
