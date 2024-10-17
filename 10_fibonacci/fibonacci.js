2648890b0ee0e7560c1c0ea1dc421465d9d4cf21
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-10-17 11:50:14 +0100

    Add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberInput) {
+    let number = parseInt(numberInput);
+    let fibonacciArray = [1, 1];
+    function fibonacciNext(fibonacciArray) {
+        return fibonacciArray[fibonacciArray.length-1] + fibonacciArray[fibonacciArray.length-2];
+    }
+    while (fibonacciArray.length < number) {
+        fibonacciArray.push(fibonacciNext(fibonacciArray));
+    }
+    if (number == 0) {
+        return 0;
+    } else if (number < 0) {
+        return "OOPS";
+    } else {
+        return fibonacciArray[number-1];
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
