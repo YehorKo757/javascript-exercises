d22c45f00d3653f36824dd9c1f80ecd53e6f0cfa
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-10-17 12:44:31 +0100

    Add solution for 12_findTheOldest
    Whooh, that was tricky. Always check how tests are built.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    let currentYear = new Date().getFullYear();
+    let result = array.map((person) => {
+        if (!person.yearOfDeath) {
+            person.age = currentYear - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        return person;
+    })
+    .sort((a, b) => {
+        if (a.age > b.age) {
+            return 1;
+        } else {
+            return -1;
+        }
+    })
+    return result[result.length-1];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
