2014998e9e25fffe82054e61b23181c13c8d96be
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-10-17 11:56:52 +0100

    Add solution for 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayInp) {
+    let array = arrayInp;
+    let result = array.map((element) => element.title);
+    return result;
 };
 
 // Do not edit below this line
